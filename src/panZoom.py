29fd1ef2abd9d5f202269085594c677f2eeea306
Gordon Brüggemann <gb@gb-web.de>
2024-01-18 21:55:04 +0100

    add zoom,position and left audio

--- src/panZoom.py
+++ src/panZoom.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 from PyQt5.QtCore import Qt, pyqtSignal
-from PyQt5.QtGui import QPainter, QPalette
-from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox,
-                             QDoubleSpinBox, QFormLayout, QHBoxLayout, QLabel,
-                             QLineEdit, QPushButton, QSlider, QSpinBox,
-                             QTabWidget, QVBoxLayout, QWidget)
-
+from PyQt5.QtGui import QPainter
+from PyQt5.QtWidgets import (QFormLayout, QLabel,
+                             QWidget)
+import numpy as np
 
 
 class panZoomDisplay(QWidget):
     updated = pyqtSignal()
-
-    def __init__(self):
+        
+    def __init__(self,picam2):
         super().__init__()
+        self.picam2 = picam2
+        self.scaler_crop = self.picam2.camera_properties["ScalerCropMaximum"]
         self.setMinimumSize(201, 151)
-        self.scale = 200 / picam2.camera_properties["ScalerCropMaximum"][2]
+        self.scale = 200 / self.picam2.camera_properties["ScalerCropMaximum"][2]
         self.zoom_level_ = 1.0
-        self.max_zoom = 7.0
+        self.max_zoom = 10.0
         self.zoom_step = 0.1
 
     @property
@@ -35,7 +35,7 @@ class panZoomDisplay(QWidget):
     def paintEvent(self, event):
         painter = QPainter()
         painter.begin(self)
-        full_img = picam2.camera_properties["ScalerCropMaximum"]
+        full_img = self.picam2.camera_properties["ScalerCropMaximum"]
         self.scale = 200 / full_img[2]
         # Whole frame
         scaled_full_img = [int(i * self.scale) for i in full_img]
@@ -43,7 +43,8 @@ class panZoomDisplay(QWidget):
         scaled_full_img[:2] = [0, 0]
         painter.drawRect(*scaled_full_img)
         # Cropped section
-        scaled_scaler_crop = [int(i * self.scale) for i in scaler_crop]
+        print(self.scaler_crop)
+        scaled_scaler_crop = [int(i * self.scale) for i in self.scaler_crop]
         scaled_scaler_crop[0] -= origin[0]
         scaled_scaler_crop[1] -= origin[1]
         painter.drawRect(*scaled_scaler_crop)
@@ -51,13 +52,13 @@ class panZoomDisplay(QWidget):
         self.updated.emit()
 
     def draw_centered(self, pos):
-        global scaler_crop
+        #global scaler_crop
         center = [int(i / self.scale) for i in pos]
-        full_img = picam2.camera_properties["ScalerCropMaximum"]
-        w = scaler_crop[2]
-        h = scaler_crop[3]
-        x = center[0] - w // 2 + picam2.camera_properties["ScalerCropMaximum"][0]
-        y = center[1] - h // 2 + picam2.camera_properties["ScalerCropMaximum"][1]
+        full_img = self.picam2.camera_properties["ScalerCropMaximum"]
+        w = self.scaler_crop[2]
+        h = self.scaler_crop[3]
+        x = center[0] - w // 2 + self.picam2.camera_properties["ScalerCropMaximum"][0]
+        y = center[1] - h // 2 + self.picam2.camera_properties["ScalerCropMaximum"][1]
         new_scaler_crop = [x, y, w, h]
 
         # Check still within bounds
@@ -65,8 +66,8 @@ class panZoomDisplay(QWidget):
         new_scaler_crop[1] = min(new_scaler_crop[1], full_img[1] + full_img[3] - new_scaler_crop[3])
         new_scaler_crop[0] = max(new_scaler_crop[0], full_img[0])
         new_scaler_crop[0] = min(new_scaler_crop[0], full_img[0] + full_img[2] - new_scaler_crop[2])
-        scaler_crop = tuple(new_scaler_crop)
-        picam2.controls.ScalerCrop = scaler_crop
+        self.scaler_crop = tuple(new_scaler_crop)
+        self.picam2.controls.ScalerCrop = self.scaler_crop
         self.update()
 
     def mouseMoveEvent(self, event):
@@ -75,14 +76,14 @@ class panZoomDisplay(QWidget):
         self.draw_centered(pos)
 
     def setZoom(self):
-        global scaler_crop
+        #global scaler_crop
         if self.zoom_level < 1:
             self.zoom_level = 1.0
         if self.zoom_level > self.max_zoom:
             self.zoom_level = self.max_zoom
         factor = 1.0 / self.zoom_level
-        full_img = picam2.camera_properties["ScalerCropMaximum"]
-        current_center = (scaler_crop[0] + scaler_crop[2] // 2, scaler_crop[1] + scaler_crop[3] // 2)
+        full_img = self.picam2.camera_properties["ScalerCropMaximum"]
+        current_center = (self.scaler_crop[0] + self.scaler_crop[2] // 2, self.scaler_crop[1] + self.scaler_crop[3] // 2)
         w = int(factor * full_img[2])
         h = int(factor * full_img[3])
         x = current_center[0] - w // 2
@@ -93,8 +94,8 @@ class panZoomDisplay(QWidget):
         new_scaler_crop[1] = min(new_scaler_crop[1], full_img[1] + full_img[3] - new_scaler_crop[3])
         new_scaler_crop[0] = max(new_scaler_crop[0], full_img[0])
         new_scaler_crop[0] = min(new_scaler_crop[0], full_img[0] + full_img[2] - new_scaler_crop[2])
-        scaler_crop = tuple(new_scaler_crop)
-        picam2.controls.ScalerCrop = scaler_crop
+        self.scaler_crop = tuple(new_scaler_crop)
+        self.picam2.controls.ScalerCrop = self.scaler_crop
         self.update()
 
     def wheelEvent(self, event):
@@ -107,19 +108,20 @@ class panZoomDisplay(QWidget):
 
 
 class panTab(QWidget):
-    def __init__(self):
+    def __init__(self, picam2):
         super().__init__()
         # Pan/Zoom
         self.layout = QFormLayout()
         self.setLayout(self.layout)
-
+        self.picam2 = picam2
+                
         self.label = QLabel((
             "Pan and Zoom Controls\n"
             "To zoom in/out, scroll up/down in the display below\n"
             "To pan, click and drag in the display below"),
             alignment=Qt.AlignCenter)
         self.zoom_text = QLabel("Current Zoom Level: 1.0", alignment=Qt.AlignCenter)
-        self.pan_display = panZoomDisplay()
+        self.pan_display = panZoomDisplay(self.picam2)
         self.pan_display.updated.connect(lambda: self.zoom_text.setText(
             f"Current Zoom Level: {self.pan_display.zoom_level:.1f}x"))
 
@@ -127,4 +129,5 @@ class panTab(QWidget):
         self.layout.addRow(self.zoom_text)
         self.layout.addRow(self.pan_display)
         self.layout.setAlignment(self.pan_display, Qt.AlignCenter)
+
 
--- src/picamRecording.py
+++ src/picamRecording.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 import time
+
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import (QApplication, QHBoxLayout, QLabel, QPushButton,
-                             QVBoxLayout, QWidget)
+                             QVBoxLayout, QWidget,QTabWidget)
 
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder, Quality
 from picamera2.outputs import FfmpegOutput
 from picamera2.previews.qt import QGlPicamera2
 
+from panZoom import panTab
 
 frame_rate = 60
 picam2_list = []
@@ -17,36 +19,32 @@ filenames = [
     'left',
     'right'
 ]
+
 recording = False
+
+camara_list = Picamera2.global_camera_info()
+for i in range(len(camara_list)):
+    picam2_list.append(Picamera2(i))
+
 app = QApplication([])
 layout_preview = QHBoxLayout()
 layout_info = QHBoxLayout()
-def post_callback(request):
-    label = QLabel()
-    label.setFixedWidth(400)
-    label.setAlignment(QtCore.Qt.AlignTop)
-    label.setText(''.join(f"{k}: {v}\n" for k, v in request.get_metadata().items()))
-    layout_info.addWidget(label)
 
-camara_list = Picamera2.global_camera_info()
-
-for i in range(0,len(camara_list)):
-    picam2_list.append(Picamera2(i))
-#   config_list.append(picam2_list[i].still_configuration())
-#    picam2_list[i].post_callback = post_callback
-    picam2_list[i].configure(picam2_list[i].create_video_configuration(main={"size": (1920, 1080)},lores={"size": (1280,720)}))
-    picam2_list[i].set_controls({"AfMode": 2 ,"AfTrigger": 0})
-    layout_preview.addWidget( QGlPicamera2(picam2_list[i], width=800, height=480, keep_ar=False), 50)
-
+for picam2 in picam2_list:
+    picam2.configure(picam2.create_video_configuration(main={"size": (1920, 1080)}, lores={"size": (1280, 720)}))
+    picam2.set_controls({"AfMode": 2, "AfTrigger": 0})
+    layout_preview.addWidget(QGlPicamera2(picam2, width=800, height=480, keep_ar=False), 50)
 
 def on_button_clicked():
     global recording
     if not recording:
-        filename = f"Videos/{filenames[i]}{int(time.time())}.mp4"
-        encoder = H264Encoder(10000000)
-        output = FfmpegOutput(filename)
+        i = 0
         for picam2 in picam2_list:
+            filename = f"Videos/{filenames[i]}{int(time.time())}.mp4"
+            encoder = H264Encoder(10000000)
+            output = FfmpegOutput(filename, audio=True)    
             picam2.start_encoder(encoder, output, quality=Quality.HIGH)
+            i=i+1
 
         button.setText("Stop recording")
         recording = True
@@ -60,19 +58,37 @@ def on_button_clicked():
 button = QPushButton("Start recording")
 button.clicked.connect(on_button_clicked)
 
+pan_zoom_list = []
+for picam2 in picam2_list:
+    pan_zoom_list.append( panTab(picam2))
+    
+layout_zoom = QHBoxLayout()
+for pan_tab in pan_zoom_list:
+    layout_zoom.addWidget(pan_tab,25)
+
 layout_base = QVBoxLayout()
 layout_base.addWidget(button)
 layout_base.addLayout(layout_preview, 100)
 layout_base.addLayout(layout_info, 50)
-
+layout_base.addLayout(layout_zoom,100)
 
 window = QWidget()
 window.setWindowTitle("Video Recording App")
 window.resize(1200, 720)
 window.setLayout(layout_base)
 
+
+p=0
 for picam2 in picam2_list:
     picam2.start()
+    pan_zoom_list[p].pan_display.update()
+    p = p+1
 
 window.show()
 app.exec()
+
+
+
+
+
+
